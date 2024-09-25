94c6e4bf52f552c8efd17e933fa8c9775229e41a
Canumay, Jhomar, C. <93630047+ExorCist21@users.noreply.github.com>
2024-09-26 04:15:43 +0800

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,7 +8,8 @@ class AdminController extends Controller
 {
     public function index()
     {
-    return view('admin.dashboard');
+        $contents = Content::all(); // Fetch all contents
+        return view('admin.dashboard', compact('contents')); // Pass contents to the view
     }
 
 }
